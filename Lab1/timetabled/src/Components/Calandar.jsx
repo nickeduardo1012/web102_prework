fa74e5b2f6b6ca6c12d98dd26821175b6434d83d
nickeduardo1012 <sanchezn2021@fau.edu>
2025-01-14 18:08:25 -0500

    Added the Calandar component.

---
+++ Lab1/timetabled/src/Components/Calandar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Calendar = () => {
+ 
+    return (
+
+        <div className="Calendar">Testing the Calendar Component</div>
+
+    )
+    
+}
+
+export default Calendar;
